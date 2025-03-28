674df693932fb45a37444ee91920ef1ed14cc14b
honestmantech <90090490+honestmantech@users.noreply.github.com>
2025-03-28 16:37:50 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,16 @@
 import { createClient } from "@supabase/supabase-js"
+import type { Database } from "@/types/database.types"
 
-const supabaseUrl = process.env.DATABASE_URL || ""
-const supabaseKey = process.env.JWT_SECRET || ""
+const supabaseUrl = process.env.DATABASE_URL || "https://zktivtcqmuhqlwpgbdzq.supabase.co"
+const supabaseKey = process.env.JWT_SECRET || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprdGl2dGNxbXVocWx3cGdiZHpxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzA0NzE5MywiZXhwIjoyMDU4NjIzMTkzfQ.iT_noPIlpLKBKailDPwajxmAtHQd3SzcJBj_WrOhbcc"
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("Missing Supabase environment variables")
 }
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
 export default supabase
+
 
